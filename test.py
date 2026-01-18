84b9e211e55956965dd6465724a4dd5bb308dee6
Paul Tarvydas <paul.tarvydas@gmail.com>
2026-01-18 09:16:34 -0500

    Ball class

--- test.py
+++ test.py
@@ -3,16 +3,23 @@ import os
 import time
 sys.path.insert(0, './pbp/kernel')
 import kernel0d as zd
+import json
 
+class Ball:
+    def __init__(self, initial_x, initial_y, initial_color, initial_dir):
+        self.x = initial_x
+        self.y = initial_y
+        self.color = initial_color
+        self.dir = initial_dir
+        
 class WH:
     def __init__(self):
-        self.x = 0
+        self.balls = []
+        self.paddle_x = 0
         self.paddle_y = 50
-        self.y = 100
         self.width = None
         self.height = None
         self.state = 'idle'
-        self.dir = 1
         self.step_size = None
 
 def handler (eh,mev):
@@ -25,17 +32,22 @@ def handler (eh,mev):
         elif 'height' == mev.port:
             self.height = int (mev.datum.v)
         elif 'rev' == mev.port:
-            self.dir = -1 * self.dir
-            self.x = self.x + (self.dir * self.step_size)
+            self.balls [0].dir = -1 * self.balls [0].dir
+            self.balls [0].x = self.balls [0].x + (self.balls [0].dir * self.step_size)
         elif '' == mev.port:
             if self.state == 'idle':
+                ball1 = Ball (0, 100, "#ffff00", 1)
+                self.balls = [ ball1 ]
                 # send paddle image if first time
-                zd.send (eh, "", '{' + f'"type":"paddle","id":"left","x":{self.x},"y":{self.paddle_y}' + '}', mev)
-                zd.send (eh, "x", f'{self.x}' , mev)
+                zd.send (eh, "", '{' + f'"type":"paddle","id":"left","x":{self.paddle_x},"y":{self.paddle_y}' + '}', mev)
+                zd.send (eh, "x", f'{ball1.x}' , mev)
                 self.state = 'looping'
-            self.x = self.x + (self.dir * self.step_size)
-            zd.send (eh, "", "{" + f'"type":"ball","x":"{self.x}","y":{self.y},"color":"#ff0000"' + "}" , mev)
-            zd.send (eh, "x", f'{self.x}' , mev)
+            self.balls [0].x = self.balls[0].x + (self.balls [0].dir * self.step_size)
+            ballpositions = []
+            for ball in self.balls:
+                ballpositions.append ({"type": "ball", "x" : ball.x, "y" : ball.y, "color": ball.color})
+            zd.send (eh, "", json.dumps (ballpositions), mev)
+            zd.send (eh, "x", f'{self.balls [0].x}' , mev)
         else:
              zd.send (eh, "✗", f"unrecognized port '{mev.port}' in test.py", mev)
     except Exception as err:
