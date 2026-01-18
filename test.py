e68c74c44c874b7e5369cfed468267de5aac416c
Paul Tarvydas <paul.tarvydas@gmail.com>
2026-01-18 09:21:17 -0500

    step_size moved to Ball

--- test.py
+++ test.py
@@ -11,6 +11,7 @@ class Ball:
         self.y = initial_y
         self.color = initial_color
         self.dir = initial_dir
+        self.step_size = None
         
 class WH:
     def __init__(self):
@@ -20,29 +21,26 @@ class WH:
         self.width = None
         self.height = None
         self.state = 'idle'
-        self.step_size = None
 
 def handler (eh,mev):
     self = eh.instance_data
     try:
         if 'width' == mev.port:
             self.width = int (mev.datum.v)
-            self.step_size = 19
-            zd.send (eh, "step size", str (self.step_size), mev)
+            self.balls [0].step_size = 19
+            zd.send (eh, "step size", str (self.balls [0].step_size), mev)
         elif 'height' == mev.port:
             self.height = int (mev.datum.v)
         elif 'rev' == mev.port:
             self.balls [0].dir = -1 * self.balls [0].dir
-            self.balls [0].x = self.balls [0].x + (self.balls [0].dir * self.step_size)
+            self.balls [0].x = self.balls [0].x + (self.balls [0].dir * self.balls [0].step_size)
         elif '' == mev.port:
             if self.state == 'idle':
-                ball1 = Ball (0, 100, "#ffff00", 1)
-                self.balls = [ ball1 ]
                 # send paddle image if first time
                 zd.send (eh, "", '{' + f'"type":"paddle","id":"left","x":{self.paddle_x},"y":{self.paddle_y}' + '}', mev)
-                zd.send (eh, "x", f'{ball1.x}' , mev)
+                zd.send (eh, "x", f'{self.balls [0].x}' , mev)
                 self.state = 'looping'
-            self.balls [0].x = self.balls[0].x + (self.balls [0].dir * self.step_size)
+            self.balls [0].x = self.balls[0].x + (self.balls [0].dir * self.balls [0].step_size)
             ballpositions = []
             for ball in self.balls:
                 ballpositions.append ({"type": "ball", "x" : ball.x, "y" : ball.y, "color": ball.color})
@@ -56,6 +54,8 @@ def handler (eh,mev):
 def instantiate (reg,owner,name, arg, template_data):
     name_with_id = zd.gensymbol ( "Test")
     self = WH ()
+    ball1 = Ball (0, 100, "#ffff00", 1)
+    self.balls = [ ball1 ]
     return zd.make_leaf ( name_with_id, owner, self, arg, handler)
 
 # define template
