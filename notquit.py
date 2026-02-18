9590eb239160423fd1f8cb11f02154b1810d73c9
Paul Tarvydas <paul.tarvydas@gmail.com>
2026-02-17 22:28:03 -0500

    not quit look at mev.datum.v

--- notquit.py
+++ notquit.py
@@ -3,7 +3,7 @@ import sys
 import kernel0d as zd
 
 def handler (eh,mev):
-    if mev.port != 'x':
+    if mev.datum.v != b'x':
         zd.forward (eh, "", mev)
     else:
         pass
@@ -15,7 +15,6 @@ def instantiate (reg,owner,name, arg, template_data):
     name_with_id = zd.gensymbol ( "not quit")
     self = None
     eh = zd.make_leaf ( name_with_id, owner, self, arg, handler, reset_handler)
-    zd.set_active (eh)
     return eh
 
 # define template
