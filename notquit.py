60fbac69f5563aeb0c3187e092371ed204497821
Paul Tarvydas <paul.tarvydas@gmail.com>
2026-02-17 22:00:24 -0500

    save

---
+++ notquit.py
@@ -0,0 +1,25 @@
+import json
+import sys
+import kernel0d as zd
+
+def handler (eh,mev):
+    if mev.port != 'x':
+        zd.forward (eh, "", mev)
+    else:
+        pass
+    
+def reset_handler (eh):
+    pass
+
+def instantiate (reg,owner,name, arg, template_data):
+    name_with_id = zd.gensymbol ( "not quit")
+    self = None
+    eh = zd.make_leaf ( name_with_id, owner, self, arg, handler, reset_handler)
+    zd.set_active (eh)
+    return eh
+
+# define template
+def install (reg):
+    zd.register_component (reg, zd.mkTemplate ("not quit", None, instantiate))
+
+
