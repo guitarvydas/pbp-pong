aa15d1c63cecab9c46c9d57e0acba4d83f5c674c
Paul Tarvydas <paul.tarvydas@gmail.com>
2026-02-17 23:39:41 -0500

    leftup, leftdown q,a

--- cdecode.py
+++ cdecode.py
@@ -5,20 +5,24 @@ import kernel0d as zd
 def handler (eh,mev):
     if mev.datum.v == '\x03':
         zd.send (eh, "quit", "", mev)
+    elif mev.datum.v == 'q':
+        zd.send (eh, "leftup", "", mev)
+    elif mev.datum.v == 'a':
+        zd.send (eh, "leftdown", "", mev)
     else:
-        zd.forward (eh, "", mev)
+        pass
     
 def reset_handler (eh):
     pass
 
 def instantiate (reg,owner,name, arg, template_data):
-    name_with_id = zd.gensymbol ( "not quit")
+    name_with_id = zd.gensymbol ( "cdecode")
     self = None
     eh = zd.make_leaf ( name_with_id, owner, self, arg, handler, reset_handler)
     return eh
 
 # define template
 def install (reg):
-    zd.register_component (reg, zd.mkTemplate ("not quit", None, instantiate))
+    zd.register_component (reg, zd.mkTemplate ("cdecode", None, instantiate))
 
 
